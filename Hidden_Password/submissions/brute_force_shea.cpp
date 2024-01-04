af0e828013b1d2e95eb3bbdcd38518fc10372f4e
Shea Janke <sheajanke@gmail.com>
2024-01-03 22:48:31 -0800

    Add 'Hidden Password' problem

---
+++ Hidden_Password/submissions/brute_force_shea.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+bool isWordInGrid(vector<string> &grid, string word) {
+    int n = grid.size();
+    int sz = word.size();
+    for(int startX = 0; startX < n; startX++){
+        for(int startY = 0; startY < n; startY++){
+            for(int dx = -1; dx <= 1; dx++){
+                for(int dy = -1; dy <= 1; dy++){
+                    if(dx == 0 && dy == 0){
+                        continue;
+                    }
+                    int curX = startX;
+                    int curY = startY;
+                    bool works = true;
+                    for(int i = 0; i < sz; i++){
+                        if(curX < 0 || curX >= n || curY < 0 || curY >= n){
+                            works = false;
+                            break;
+                        }
+                        if(grid[curX][curY] != word[i]){
+                            works = false;
+                        }
+                        curX += dx;
+                        curY += dy;
+                    }
+                    if(works){
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+
+    int n;
+    cin >> n;
+    vector<string> grid(n);
+    for(int i = 0; i < n; i++){
+        cin >> grid[i];
+    }
+    int m;
+    cin >> m;
+    vector<string> potentialPasswords(m);
+    for(int i = 0; i < m; i++){
+        cin >> potentialPasswords[i];
+    }
+
+    for(int i = 0; i < m; i++){
+        if(isWordInGrid(grid, potentialPasswords[i])){
+            cout << potentialPasswords[i] << endl;
+            exit(0);
+        }
+    }
+}
